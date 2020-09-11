18b647bbb1ef47dc02d5206e77773dd14b19cbb6
kaylawilson535 <42094645+kaylawilson535@users.noreply.github.com>
2020-09-11 14:15:15 -0600

    Added Eli's scripts for Oil AI

---
+++ Bacon Frenzy/Assets/Scripts/OilAI/OilMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilMove : OilState
+{
+    float oilDuration;
+    Vector3 targetSize = new Vector3 (0,0,0);
+    float shrinkSpeed = 1f;
+    float shrinkDuration = 5;
+    GameObject oil;
+
+    public OilMove(OilStateController stateController) : base(stateController)
+    {
+
+    }
+    public override void CheckTransitions()
+    {
+        //After random pos is located : start timer : when timer ends : switch to OilStop state
+        if (shrinkDuration <= 0.0f)
+        {
+            stateController.SetState(new OilStop(stateController));
+        }
+    }
+
+    public override void Act()
+    {
+        //Shrink in size : get area of play area and random a position : Then either switch to OilStop state or
+        //wait x seconds (could be incremented with duration of game increasing difficulty) : Then switch
+        shrinkDuration -= Time.deltaTime;
+      
+        oil.transform.localScale = Vector3.Lerp(oil.transform.localScale, targetSize, shrinkSpeed * Time.deltaTime);
+
+        
+
+    }
+    public override void OnStateEnter()
+    {
+
+        oil = stateController.thisObj;
+    }
+}
---
+++ Bacon Frenzy/Assets/Scripts/OilAI/OilStop.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OilStop : OilState
+{
+    float oilDuration;
+    Vector3 targetSize = new Vector3(3, .01f, 3);
+    float growSpeed = .2f;
+    float growDuration = 10;
+    GameObject oil;
+
+    public OilStop(OilStateController stateController) : base(stateController)
+    {
+
+    }
+    public override void CheckTransitions()
+    {
+        //See if timer is up, then change to OilMove state.
+        if (oilDuration < 0.0f)
+            stateController.SetState(new OilMove(stateController));
+    }
+
+    public override void Act()
+    {
+        // Grow in size
+        // Once growDuration is reached => stop growing => start oilDuration timer
+
+        growDuration -= Time.deltaTime;
+        if (growDuration <= 0.0f)
+        {
+            oilDuration -= Time.deltaTime;
+            Debug.Log(oilDuration);
+
+        }
+        else
+        {
+            oil.transform.localScale = Vector3.Lerp(oil.transform.localScale, targetSize, growSpeed * Time.deltaTime);
+        }
+    }
+    public override void OnStateEnter()
+    {
+        // Get random num in range : use it as the num of seconds this state
+
+        oil = stateController.thisObj;
+        oilDuration = UnityEngine.Random.Range(3.0f, 7.0f);
+        Debug.Log("Initial oilDuration: " + oilDuration);
+    }
+}
